9ba2a342c21dc53b6833d27f86a503750e8ce599
algassby <algassby@hotmail.com>
2020-12-06 21:43:13 +0100

    spring boot rest jwt auth

---
+++ src/test/java/com/barry/spring/MySpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barry.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
